b45d03f030d1252d9070d0605d300e8b5285e865
Quix <nswells14@gmail.com>
2019-02-07 00:59:56 +0200

    Added index.erb

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
 
 get '/new' do
@@ -38,7 +38,7 @@ post '/new' do
 				@error = 'Type text'
 				return erb :new
 		end
-
+# сохраненеи данных в БД
 		@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
 
