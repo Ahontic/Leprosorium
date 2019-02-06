0b4bdf5b1dbaf965d563c2901bfdb09b4035c9c5
Quix <nswells14@gmail.com>
2019-02-07 00:52:15 +0200

    Added parameter validation

--- app.rb
+++ app.rb
@@ -32,6 +32,12 @@ get '/new' do
 end
 
 post '/new' do
-		@content = params[:content]
-		erb "You typed #{@content}"
+		content = params[:content]
+
+		if content.length <= 0
+				@error = 'Type text'
+				return erb :new
+		end
+
+		erb "You typed #{content}"
 	end
