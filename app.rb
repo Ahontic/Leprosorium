0ef2d5baea8646c4ba059652271213185b40db82
Quix <nswells14@gmail.com>
2019-02-07 02:33:14 +0200

    Save comment to db

--- app.rb
+++ app.rb
@@ -20,15 +20,15 @@ configure do
 	(  
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
     created_date DATE,
-    content TEXT,
-    post_id INTEGER
-)'
+    content TEXT
+    )'
 
 @db.execute 'CREATE TABLE IF NOT EXISTS Comments
 	(  
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
     created_date DATE,
-    content TEXT
+    content TEXT,
+    post_id INTEGER
 )';
 
 end
@@ -82,7 +82,23 @@ post '/details/:post_id' do
 	post_id = params[:post_id]
 	content = params[:content]
 
-	erb "You typed comment #{content} for post #{post_id}"
+
+	@db.execute 'insert into Comments
+	(
+		content,
+		created_date,
+		post_id
+	) 
+		values
+	(
+	 	?,
+	 	datetime(),
+	 	?
+	 )', [content,post_id]
+
+	# перенаправление на страницу поста
+		redirect to('/details/' + post_id)
+		
 
 
 end
